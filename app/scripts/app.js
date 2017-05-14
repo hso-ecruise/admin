5317a864a8cd281aef155bcd9bf7407e8155d751
JenEwa <jenewa@posteo.de>
2017-05-14 16:29:34 +0200

    created controllers and routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,8 +8,7 @@
  *
  * Main module of the application.
  */
-angular
-  .module('adminApp', [
+var application = angular.module('adminApp', [
     'ngAnimate',
     'ngAria',
     'ngCookies',
@@ -18,20 +17,68 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch'
-  ])
-  .config(function ($routeProvider) {
+]);
+
+var checkRouting= function ($rootScope, $location, $cookies) {
+    
+    if ($rootScope.LoggedIN === false || $rootScope.LoggedIN === undefined)
+    {
+	if($rootScope.LoggedIN === undefined){
+	    $rootScope.LoggedIN = false;
+	}
+	
+	$location.path("/start");
+    }
+    else
+    {
+	alert("true or other");
+	alert($rootScope.LoggedIN);
+    }
+};
+
+application.config(function ($routeProvider, $locationProvider){
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+	.when('/', {
+	    templateUrl: 'views/start.html',
+	    resolve: {
+		factory: checkRouting
+	    }
+
+	})
+	.when('/about', {
+	    templateUrl: 'views/about.html',
+	    controller: 'AboutCtrl',
+	    controllerAs: 'about'
+	})
+	.when('/start', {
+	    templateUrl: 'views/start.html',
+	    controller: 'StartCtrl',
+	    controllerAs: 'start'
+	})
+    	.when('/vehicles', {
+	    templateUrl: 'views/vehicles.html',
+	    controller: 'VehiclesCtrl',
+	    controllerAs: 'vehicles'
+	})
+        .when('/users', {
+	    templateUrl: 'views/users.html',
+	    controller: 'UsersCtrl',
+	    controllerAs: 'users'
+	})
+        .when('/bookings', {
+	    templateUrl: 'views/bookings.html',
+	    controller: 'BookingsCtrl',
+	    controllerAs: 'bookings'
+	})
+        .when('/statistics', {
+	    templateUrl: 'views/statistics.html',
+	    controller: 'StatisticsCtrl',
+	    controllerAs: 'statistics'
+	})
+	.otherwise({
+	    redirectTo: '/'
+	});
+
+    $locationProvider
+	.html5Mode(true);
+});
